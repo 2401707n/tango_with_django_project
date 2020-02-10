79a19463641305489eeeb96863c99e5f11398e7f
Thanh Hieu Nguyen <2401707N@student.gla.ac.uk>
2020-02-10 00:05:47 +0000

    Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+    def __str__(self):
+        return self.name
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+
