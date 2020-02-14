071fb2fa0d9e2fbc8e1e892569887abdf4614c16
Thanh Hieu Nguyen <2401707N@student.gla.ac.uk>
2020-02-14 09:51:30 +0000

    Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -27,8 +27,6 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     visitor_cookie_handler(request)
-    context_dict["visits"] = request.session['visits']
-    context_dict["last_visit"] = request.session["last_visit"]
     
     response = render(request, 'rango/index.html', context=context_dict)
     
@@ -170,8 +168,8 @@ def get_server_side_cookie(request, cookie, default_val=None):
 
 
 def visitor_cookie_handler(request):
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    visits = int(get_server_side_cookie(request, 'visits', '1')) 
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now())) 
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
